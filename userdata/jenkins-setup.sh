87a7c44a4e06ca7bb080de6ba951878fb6e9d20c
Ndzenyuy <ndzenyuyjones@gmail.com>
2025-08-09 11:12:51 -0700

    modified jdk to 21

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
  
-sudo apt install openjdk-17-jdk -y
+sudo apt install openjdk-21-jdk -y
  
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
 https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
@@ -13,3 +13,23 @@ https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
 sudo apt-get update
  
 sudo apt-get install jenkins -y
+
+# install docker
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc -y
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+sudo usermod -aG docker $USER
+sudo usermod -aG docker jenkins
